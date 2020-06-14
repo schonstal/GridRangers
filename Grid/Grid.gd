0de42db10c32e86d8b289e9e7732fbd690f33cda
Josh Schonstal <josh@schonstal.org>
2020-06-14 15:20:02 -0700

    🔧 Add basic tile drag

--- Grid/Grid.gd
+++ Grid/Grid.gd
@@ -6,8 +6,9 @@ export var height = 24
 export(Array,Resource) var tile_scenes = []
 
 var tiles = []
-
 var tile_size = 128
+
+var selected_tile = null
 
 onready var background = $Background
 
@@ -40,6 +41,9 @@ func spawn_tile(x,y):
   instance.scale.x = 0.8
   instance.scale.y = 0.8
 
+  instance.grid_position.x = x
+  instance.grid_position.y = y
+
   call_deferred("add_child", instance)
 
   return instance
@@ -60,3 +64,71 @@ func match_type(type, x, y):
     if first != null && second != null:
       if type == first.type && first.type == second.type:
         return true
+
+func mouse_to_grid():
+  var mouse_position = get_viewport().get_mouse_position()
+  var local_position = mouse_position - global_position
+
+  if local_position.x < 0 || local_position.y < 0:
+    return null
+  if local_position.x > tile_size * width || local_position.y > tile_size * height:
+    return null
+
+  var grid_position = local_position / tile_size
+
+  grid_position.x = int(grid_position.x)
+  grid_position.y = int(grid_position.y)
+
+  return grid_position
+
+func grab_tile(grid_position):
+  selected_tile = tiles[grid_position.x][grid_position.y]
+  selected_tile.start_drag()
+
+func swap_tile(grid_position):
+  if selected_tile == null:
+    return
+
+  selected_tile.stop_drag()
+
+  if !legal_swap(selected_tile.grid_position, grid_position):
+    selected_tile.position.x = tile_size * selected_tile.grid_position.x + tile_size / 2
+    selected_tile.position.y = tile_size * selected_tile.grid_position.y + tile_size / 2
+    selected_tile = null
+    return
+
+  var other = tiles[grid_position.x][grid_position.y]
+
+  tiles[grid_position.x][grid_position.y] = selected_tile
+  tiles[selected_tile.grid_position.x][selected_tile.grid_position.y] = other
+
+  selected_tile.position.x = tile_size * grid_position.x + tile_size / 2
+  selected_tile.position.y = tile_size * grid_position.y + tile_size / 2
+
+  other.position.x = tile_size * selected_tile.grid_position.x + tile_size / 2
+  other.position.y = tile_size * selected_tile.grid_position.y + tile_size / 2
+  other.grid_position.x = selected_tile.grid_position.x
+  other.grid_position.y = selected_tile.grid_position.y
+
+  selected_tile.grid_position.x = grid_position.x
+  selected_tile.grid_position.y = grid_position.y
+
+  selected_tile = null
+
+func legal_swap(start, end):
+  var difference = start - end; 
+  return abs(difference.x) + abs(difference.y) == 1
+
+func _input(event):
+  if event is InputEventMouseButton:
+    var location = mouse_to_grid()
+    if location == null:
+      print("outside grid")
+      return
+
+    if event.button_index == BUTTON_LEFT && event.pressed:
+      grab_tile(mouse_to_grid())
+      print("clicked on ", mouse_to_grid())
+    elif event.button_index == BUTTON_LEFT and !event.pressed:
+      print("released at ", mouse_to_grid())
+      swap_tile(mouse_to_grid())
--- Tiles/Tile.gd
+++ Tiles/Tile.gd
@@ -1,6 +1,40 @@
-extends Node2D
+extends Area2D
 
 export var type = "Default"
 
+var grid_position = Vector2()
+
+var dragging = false
+var drag_offset = Vector2()
+
+var origin_position = Vector2()
+
 func _ready():
-  pass
+  connect("input_event", self, "_on_Tile_input_event")
+
+func _process(delta):
+  if dragging:
+    drag()
+
+func drag():
+  var mouse_position = get_viewport().get_mouse_position()
+
+  var new_position = Vector2(
+      mouse_position.x,
+      mouse_position.y
+    ) + drag_offset
+
+  if abs(origin_position.x - new_position.x) > abs(origin_position.y - new_position.y):
+    global_position.x = max(min(new_position.x, origin_position.x + 128), origin_position.x - 128)
+    global_position.y = origin_position.y
+  else:
+    global_position.x = origin_position.x
+    global_position.y = max(min(new_position.y, origin_position.y + 128), origin_position.y - 128)
+
+func start_drag():
+  dragging = true
+  drag_offset = global_position - get_viewport().get_mouse_position()
+  origin_position = global_position
+
+func stop_drag():
+  dragging = false
