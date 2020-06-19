13e34458e6a4db5e54538bbb0339da1d704372f4
Josh Schonstal <josh@schonstal.org>
2020-06-18 20:13:37 -0700

    ✨ Improve pig head UI

--- UI/PIGCount.gd
+++ UI/PIGCount.gd
@@ -1,6 +1,22 @@
-extends Sprite
+extends Node2D
 
-onready var label = $Label
+onready var label = $Badge/Label
+onready var badge = $Badge
+onready var animation = $AnimationPlayer
+onready var sprite = $Head
+
+func _ready():
+  EventBus.connect("enemy_died", self, "_on_enemy_died")
+  
+func _on_enemy_died(_e):
+  if Game.scene.kills >= Game.scene.kill_target:
+    badge.visible = false
+    sprite.modulate = Color(10, 10, 10, 1)
+  else:
+    animation.play("Increase")
 
 func _process(delta):
-  label.text = "%0d/%d" % [Game.scene.kills, Game.scene.kill_target]
+  if Game.scene.kills < Game.scene.kill_target:
+    label.text = "%0d/%d" % [Game.scene.kills, Game.scene.kill_target]
+  else:
+    label.text = "%0d/%d" % [Game.scene.kill_target, Game.scene.kill_target]
