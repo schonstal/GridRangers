d3d5adf35f1f7b18e232f51a6e43bd54b5b6e3ba
Josh Schonstal <josh@schonstal.org>
2020-06-20 21:13:19 -0700

    🔧 Remove debug print

--- Tiles/Tile.gd
+++ Tiles/Tile.gd
@@ -68,7 +68,6 @@ func stop_drag():
   dragging = false
 
 func hurt(damage):
-  print(grid_position, "ouch", damage)
   health -= damage
   emit_signal("hurt")
   EventBus.emit_signal("explode", global_position)
