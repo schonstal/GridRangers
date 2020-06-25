dfbbfeda3b9a584b74a94636dcfda6d0480e75ca
Josh Schonstal <josh@schonstal.org>
2020-06-25 11:08:23 -0700

    🐛 Fix ability costs and buggy effects

--- Abilities/FullPlus.gd
+++ Abilities/FullPlus.gd
@@ -4,7 +4,7 @@ func activate_at(position):
   for x in Game.scene.grid.width:
     for y in Game.scene.grid.height:
       if position.x == x || position.y == y:
-        var tile = Game.scene.grid.get_tile(Vector2(position.x, y))
+        var tile = Game.scene.grid.get_tile(x, y)
         if tile.player == false:
           tile.hurt(1)
 
