12dfc71145ec1e883babe6d49cae69fd76964988
Josh Schonstal <josh@schonstal.org>
2020-06-27 14:50:34 -0700

    💇‍♀️Clean whitespace

--- UI/GameOver.gd
+++ UI/GameOver.gd
@@ -5,7 +5,7 @@ onready var game_over_sound = $GameOverSound
 func _ready():
   EventBus.connect("game_over", self, "_on_game_over")
   visible = false
-  
+
 func _on_game_over():
   visible = true
   game_over_sound.play()
