5ada112117f1c5ed7daa49a2399256c446897de8
Josh Schonstal <josh@schonstal.org>
2020-06-27 14:49:55 -0700

    🔧 Fix popping sound on pig death

--- SFXLimiter.gd
+++ SFXLimiter.gd
@@ -2,16 +2,11 @@ extends Node2D
 
 func _ready():
   EventBus.connect("play_sound", self, "_on_play_sound")
-  
+
 func _on_play_sound(category, sfx_name):
   if Game.scene.phase == Game.PHASE_NONE:
     return
 
-  for child in get_children():
-    var node = child.find_node(sfx_name)
-    if node != null && node.has_method("stop"):
-      node.stop()
-  
   var child = find_node(category)
   if category != null:
     var node = child.find_node(sfx_name)
