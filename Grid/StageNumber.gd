ab13f512e951bf11d16c608e257871fa11eae12f
Josh Schonstal <josh@schonstal.org>
2020-06-30 15:19:40 -0700

    🔧 Make StageNumber say final stage

--- Grid/StageNumber.gd
+++ Grid/StageNumber.gd
@@ -4,4 +4,9 @@ func _ready():
   EventBus.connect("start_level", self, "_on_start_level")
 
 func _on_start_level():
-  text = "STAGE %d" % [Game.scene.level_index + 1] 
+  var level = Game.scene.level_index + 1
+
+  if level >= Game.scene.levels.size():
+    text = "FINAL STAGE"
+  else:
+    text = "STAGE %d" % level
